63ea5aba63476e1428e2cd97110c4b2dbd31aa97
Iryna-source <ilogvinenko@ukr.net>
2025-01-22 21:06:04 +0200

    hw01
    Версія1

---
+++ hw01/hw1.js
@@ -0,0 +1,19 @@
+// Завдання 1
+
+let productCount, productName;
+productCount = 10;
+productName = "Milk";
+productCount = 15;
+productName = "Juice";
+console.log(productCount);
+console.log(productName);
+
+// Завдання 2
+
+let userSalary, userPosition;
+userSalary = 1000;
+userPosition = "Junior Developer";
+userSalary = 1500;
+userPosition = "Middle Developer";
+console.log(userSalary);
+console.log(userPosition);
